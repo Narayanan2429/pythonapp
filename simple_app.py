2748dbc42a062ddf73be37072bd406e94ab86a9a
Narayanan <narayanan.bk@gmail.com>
2025-09-06 09:59:11 +0530

    Added simple_app.py

--- simple_app.py
+++ simple_app.py
@@ -1,1 +1,6 @@
 print("Hello, GitHub...by Dr.B.NARAYANAN")
+print("I am a learner")
+num1=float(input("Enter the first number:"))
+num2=float(input("Enter the second number:"))
+sum=num1+num2
+print("The sum of two numbers is", sum)
